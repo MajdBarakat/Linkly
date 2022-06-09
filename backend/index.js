8784a6c8c253012b91587146835fa8b90c109ebd
Majd Barakat <78052909+MajdBarakat@users.noreply.github.com>
2022-06-09 10:59:45 +0100

    backend main setup

---
+++ backend/index.js
@@ -0,0 +1,14 @@
+// require("express-async-errors");
+const env = require("dotenv").config();
+const express = require("express");
+const app = express();
+
+require("./startup/routes")(app);
+
+require("./startup/db")();
+
+//logging errors with winston
+
+//PORT
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
