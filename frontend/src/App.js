e6683862d7dfdc67de2a776baa778493f82f5a79
Majd Barakat <78052909+MajdBarakat@users.noreply.github.com>
2022-06-14 00:36:18 +0100

    Started React App/ Frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,10 @@
+import logo from "./logo.svg";
+import "./App.css";
+import axios from "axios";
+import LoginFrom from "./components/common/loginForm";
+
+function App() {
+  return <LoginFrom />;
+}
+
+export default App;
