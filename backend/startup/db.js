33047d9f55fd2f9970140612b00deba4d4980902
Majd Barakat <78052909+MajdBarakat@users.noreply.github.com>
2023-01-19 17:12:29 +0400

    setup on mac-os, changed user model slightly (bloat)

--- backend/startup/db.js
+++ backend/startup/db.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 module.exports = function () {
   //mongodb
   mongoose
-    .connect("mongodb://localhost:27017/playground")
+    .connect("mongodb://127.0.0.1/playground")
     .then(() => console.log("Connected to mongoDB"))
     .catch((err) => console.log("ERROR", err));
 };
