1b99d3f74d7b57d57a5c8015e0eadb27b8edf0d4
Majd Barakat <78052909+MajdBarakat@users.noreply.github.com>
2023-01-21 17:18:01 +0400

    textarea component

---
+++ frontend/src/components/common/textarea.jsx
@@ -0,0 +1,40 @@
+export default ({
+    name,
+    label,
+    value,
+    onChange,
+    onBlur,
+    type,
+    placeholder,
+    help,
+    error,
+  }) => {
+    return (
+      <div className={"input-container"}>
+        <label>{label}</label>
+        <textarea
+          className={error ? "error" : undefined}
+          name={name}
+          value={value}
+          type={type}
+          placeholder={placeholder}
+          onChange={onChange}
+          onBlur={onBlur}
+        ></textarea>
+        {error || help ? (
+          <div className="under-text-container">
+            <div className="text-error">{error ? error : ""}</div>
+            {help && (
+              <div className="text-help">
+                {help.text}
+                <a href={help.href}>{help.label}</a>
+              </div>
+            )}
+          </div>
+        ) : (
+          ""
+        )}
+      </div>
+      //if error is truthy returns the div of errors
+    );
+  };
