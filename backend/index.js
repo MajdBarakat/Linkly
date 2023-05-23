ce772c4bf934ae621fb199a9701ba6e6f06317c1
Majd Barakat <78052909+MajdBarakat@users.noreply.github.com>
2023-05-23 15:48:54 +0100

    preparing project for deployment

--- backend/index.js
+++ backend/index.js
@@ -10,5 +10,9 @@ require("./startup/db")();
 //logging errors with winston
 
 //PORT
-const port = process.env.PORT || 3001;
-app.listen(port, () => console.log(`Listening on port ${port}...`));
+const port = process.env.PORT;
+if (port) {
+    app.listen(port, () => console.log(`Listening on port ${port}...`));
+}
+
+module.exports = app;
