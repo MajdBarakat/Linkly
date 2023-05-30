459e45017a32493db78a937bbe904b975e87fc94
Majd Barakat <78052909+MajdBarakat@users.noreply.github.com>
2023-05-30 20:51:51 +0100

    DB config for vercel

--- backend/startup/db.js
+++ backend/startup/db.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 module.exports = function () {
   //mongodb
   mongoose
-    .connect("mongodb://127.0.0.1/playground")
+    .connect(process.env.MONGODB_URI)
     .then(() => console.log("Connected to mongoDB"))
     .catch((err) => console.log("ERROR", err));
 };
